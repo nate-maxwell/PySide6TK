cba7a65926c7078d4dbdf3f644fa48ba782000ac
nate-maxwell <natedmaxwell@gmail.com>
2026-06-14 12:38:42 -0700

    opengl to __init__

--- PySide6TK/__init__.py
+++ PySide6TK/__init__.py
@@ -1,12 +1,16 @@
 from pathlib import Path
-from typing import Optional
-from typing import Union
 
-from PySide6 import QtCore
-from PySide6 import QtGui
-from PySide6 import QtWidgets
-from PySide6 import QtOpenGL
-from PySide6 import QtOpenGLWidgets
+import PySide6.QtCore
+import PySide6.QtGui
+import PySide6.QtOpenGL
+import PySide6.QtOpenGLWidgets
+import PySide6.QtWidgets
+
+QtCore = PySide6.QtCore
+QtGui = PySide6.QtGui
+QtWidgets = PySide6.QtOpenGL
+QtOpenGL = PySide6.QtOpenGLWidgets
+QtOpenGLWidgets = PySide6.QtWidgets
 
 
 MODULE_NAME = Path(__file__).parent.name
